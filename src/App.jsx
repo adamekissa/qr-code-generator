f23f7f35b5e9ef7a68269e4e76930029bf293447
Mohammed Adam <adamekissa@gmail.com>
2024-06-28 14:05:17 +0100

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import { useState } from 'react'
+import QRCode from 'qrcode';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [qrList, setQrList] = useState([])
+  const [searchUrl, setSearchUrl] = useState([]);
 
+  function handleChange(event) {
+    setSearchUrl(event.target.value);
+  }
+
+  function generateQR() {
+    QRCode.toDataURL(searchUrl, function (err, searchUrl) {
+      setQrList([searchUrl, ...qrList])
+
+    });
+  }
   return (
     <>
       <div className='container-div'>
         <h3>Welcome to the QR code generator by Mohammed</h3>
-        Url: <input className='input-element' type='text'></input>
+        Url: <input onChange={handleChange} className='input-element' type='text'></input>
         <br />
-        <button className='generate-button'>Generate QR Code</button>
+        <button onClick={generateQR} className='generate-button'>Generate QR Code</button>
+        <br />
+        {qrList.map((element) => (<img src={element} alt="Hello" />))}
       </div>
     </>
   )
